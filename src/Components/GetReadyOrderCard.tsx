ad66c94e916691162e5899ce05a511a0cebde503
Mondli <mondli46401@gmail.com>
2026-01-30 10:13:42 +0200

    Here I just finished with the cards for meals prepared and for ready cards, just need to shared real data now

--- src/Components/GetReadyOrderCard.tsx
+++ src/Components/GetReadyOrderCard.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import "../styles/GetReadyOrderCard.css"
 
-const GetReadyOrderCard:React.FC = () => {
+interface TheProps{
+    orderNo: number;
+    mealName: string;
+    customerName: string;
+    imgUrl: string
+}
+
+const GetReadyOrderCard:React.FC<TheProps> = ({orderNo, mealName, customerName, imgUrl}) => {
   return (
     <div className='readyCardContainerG'>
-        <div className='mealImg'/>
+        <div className='mealImg' style={{backgroundImage:`url(${imgUrl})`}}/>
         <div className='mealData'>
-            <strong style={{fontSize:"20px", color:"#ffb20c"}}>23224</strong>
-            <strong style={{fontSize:"18px", lineHeight:"15px" }}>Unwise Street</strong>
-            <strong style={{fontSize:"16px", lineHeight:"15px" }}>Paole Jobe</strong>
+            <strong style={{fontSize:"20px", color:"#ffb20c"}}>{orderNo}</strong>
+            <strong style={{fontSize:"18px", lineHeight:"15px" }}>{mealName}</strong>
+            <strong style={{fontSize:"16px", lineHeight:"15px" }}>{customerName}</strong>
             <div style={{ width:"100%", height:"30%", 
                         marginTop:"2%", display:"flex", justifyContent:"end"}}>
                 <div className='readyBtnG'>
-                    <strong>Ready</strong>
+                    <strong>Preparing</strong>
                 </div>
             </div>
         </div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,16 +4,18 @@ import NewOrderCard from '../Components/NewOrderCard';
 import ReadyOrderCard from '../Components/ReadyOrderCard';
 import GetReadyOrderCard from '../Components/GetReadyOrderCard';
 import theImg from '../assets/images/image2.png'
-import theImg2 from '../assets/images/image2.png'
-import theImg3 from '../assets/images/image2.png'
+import theImg2 from '../assets/images/image.png'
+import theImg3 from '../assets/images/image3.png'
 
 const Dashboard:React.FC = () => {
   return (
     <div className='containerD'>
       <div className='leftBlock'>
         <div className='horizontCards'>
-          <NewOrderCard orderNo={24342} mealName='Unwise Street' delivery={true} quantity={3} imgUrl={theImg}/>
-
+          <NewOrderCard orderNo={24342} mealName='Unwise Street' delivery={true} quantity={1} imgUrl={theImg}/>
+          <NewOrderCard orderNo={24342} mealName='Street Wise' delivery={false} quantity={3} imgUrl={theImg2}/>
+          <NewOrderCard orderNo={24342} mealName='Dacker Dumpy' delivery={true} quantity={2} imgUrl={theImg}/>
+          <NewOrderCard orderNo={24342} mealName='Unwise Street' delivery={false} quantity={6} imgUrl={theImg3}/>
         </div>
         <div className='verticalCards'>
           <div className="vLeft">
@@ -21,7 +23,10 @@ const Dashboard:React.FC = () => {
               <strong>Ready for Collection</strong>
             </div>
             <div className='vSlider'>
-              <ReadyOrderCard/>
+              <ReadyOrderCard orderNo={33332} mealName='Dump Str-Fry' customerName='John Baloyi' imgUrl={theImg3}/>
+              <ReadyOrderCard orderNo={33542} mealName='Fried Meal' customerName='Anele Khoza' imgUrl={theImg}/>
+              <ReadyOrderCard orderNo={36432} mealName='Platter Medium' customerName='Mondli Khoza' imgUrl={theImg2}/>
+              <ReadyOrderCard orderNo={63332} mealName='Dump Str-Fry' customerName='Nkosingiplile Mbhele' imgUrl={theImg3}/>
             </div>
           </div>
           <div className="vRight">
@@ -29,7 +34,10 @@ const Dashboard:React.FC = () => {
               <strong>Ready for Collection</strong>
             </div>
             <div className='vSlider'>
-              <ReadyOrderCard/>
+              <ReadyOrderCard orderNo={63332} mealName='Dump Str-Fry' customerName='John Baloyi' imgUrl={theImg3}/>
+              <ReadyOrderCard orderNo={33542} mealName='Fried Meal' customerName='Vutomu Juliose' imgUrl={theImg}/>
+              <ReadyOrderCard orderNo={36432} mealName='Platter Medium' customerName='Mondli Khoza' imgUrl={theImg2}/>
+              <ReadyOrderCard orderNo={85432} mealName='Ribbs & Sourcage' customerName='Xoliswa Khoza' imgUrl={theImg}/>
             </div>
           </div>
         </div>
@@ -39,7 +47,12 @@ const Dashboard:React.FC = () => {
               <strong>Getting Ready</strong>
         </div>
         <div className='vSliderTwo'>
-          <GetReadyOrderCard/>
+          <GetReadyOrderCard orderNo={63332} mealName='Dump Str-Fry' customerName='John Baloyi' imgUrl={theImg3}/>
+          <GetReadyOrderCard orderNo={85432} mealName='Ribbs & Sourcage' customerName='Xoliswa Khoza' imgUrl={theImg}/>
+          <GetReadyOrderCard orderNo={33542} mealName='Fried Meal' customerName='Anele Khoza' imgUrl={theImg2}/>
+          <GetReadyOrderCard orderNo={63332} mealName='Dump Str-Fry' customerName='Nkosingiplile Mbhele' imgUrl={theImg}/>
+          <GetReadyOrderCard orderNo={33332} mealName='Dump Str-Fry' customerName='John Baloyi' imgUrl={theImg3}/>
+          <GetReadyOrderCard orderNo={36432} mealName='Platter Medium' customerName='Mondli Khoza' imgUrl={theImg2}/>
         </div>
       </div>
     </div>
